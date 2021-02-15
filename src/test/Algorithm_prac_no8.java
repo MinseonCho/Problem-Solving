70679f8755e0481e03070d3b2339006a9e92f380
MinseonCho <seoon53@gmail.com>
2021-02-15 12:21:29 +0900

    Update Algorithm_prac_no8.java

--- src/test/Algorithm_prac_no8.java
+++ src/test/Algorithm_prac_no8.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class Algorithm_prac_no8 {
 
-	static int N;
-	static int M;
-	static int[][] data;
+	static int N; // ¼¼·Î±æÀÌ
+	static int M; // °¡·Î±æÀÌ
+	static int[][] data; // ¸Ê
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -17,6 +18,7 @@ public class Algorithm_prac_no8 {
 
 		data = new int[N][M];
 
+		// ¸Ê¿¡ µ¥ÀÌÅÍ ÀúÀå
 		for (int n = 0; n < N; n++) {
 			for (int m = 0; m < M; m++) {
 				data[n][m] = sc.nextInt();
@@ -31,16 +33,17 @@ public class Algorithm_prac_no8 {
 					result++;
 			}
 		}
-		
+
 		System.out.println("result : " + result);
 
 	}
 
 	static boolean dfs(int x, int y) {
+		// ¸ÊÀ» ¹þ¾î³ª¸é result false;
 		if (x < 0 | x > N - 1 | y < 0 | y > M - 1) {
 			return false;
 		}
-		if (data[x][y] == 0) {
+		if (data[x][y] == 0) { // ±¸¸ÛÀÌ¸é
 			data[x][y] = 1; // ¹æ¹®Ã³¸®
 			dfs(x - 1, y); // up
 			dfs(x + 1, y); // down
@@ -48,7 +51,7 @@ public class Algorithm_prac_no8 {
 			dfs(x, y + 1); // right
 			return true;
 		}
-		return false;
+		return false; // ±¸¸ÛÀÌ ¾Æ´Ï¸é
 	}
 
 }
