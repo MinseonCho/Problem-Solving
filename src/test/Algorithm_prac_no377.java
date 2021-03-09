d5d0a3fc535a8d274f1d6f6c25424a61e1f34ad8
MinseonCho <seoon53@gmail.com>
2021-03-09 18:10:39 +0900

    Create Algorithm_prac_no377.java

---
+++ src/test/Algorithm_prac_no377.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.Scanner;
+//Επ»η
+public class Algorithm_prac_no377 {
+
+	static int n;
+	static int[] dp = new int[16];
+	static int[] t = new int[15];
+	static int[] p = new int[15];
+	static int maxValue;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			t[i] = sc.nextInt();
+			p[i] = sc.nextInt();
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			int time = t[i] + i;
+			if (time <= n) {
+				dp[i] = Math.max(p[i] + dp[time], maxValue);
+				maxValue = dp[i];
+			} else
+				dp[i] = maxValue;
+		}
+
+		System.out.println(maxValue);
+
+	}
+
+}
